c1fa74baf3d7f6fccfc9db76114f2e3e6ae159db
noval6 <87885361+noval6@users.noreply.github.com>
2022-06-19 20:38:33 +0530

    Add files via upload

---
+++ program kata terimakasih.py
@@ -0,0 +1,7 @@
+program = input("Kami dari mahasiswa yang di ajar mengucapkan terimakasih kepada kakak - kakak telah memberikan praktikum code ini")
+
+opens = open("D:\\Tugas Kuliah\\Ucapan_terimakasih.txt")
+
+open.write(f"Terimakasih\t: {program}")
+
+opens.close()
